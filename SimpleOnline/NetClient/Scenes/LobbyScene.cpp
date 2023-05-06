c8dcd9a16a5840d03d01d2b972fd06b0a0cff445
Caue <cauechavesvicente@gmail.com>
2023-05-06 15:11:51 -0300

    add countdown before going to game scene

--- SimpleOnline/NetClient/Scenes/LobbyScene.cpp
+++ SimpleOnline/NetClient/Scenes/LobbyScene.cpp
@@ -25,23 +25,23 @@ void lobby_scene::show()
 
 void lobby_scene::wait_player_ready(const shared_ptr<client>& client_ptr, bool ready)
 {
+    cout << "Type [OK] to toggle the ready state" << endl;
     client_ptr->read_input([&](const string& input)
     {
-        if (input == "ok")
-            ready = !ready;
-        else
+        if (input != "ok")
         {
             wait_player_ready(client_ptr, ready);
             return;
         }
 
+        ready = !ready;
         client_ptr->set_player_ready(ready, [&](const domain::lobby_domain& lobby)
         {
             print_lobby(lobby);
 
             if (lobby.start_match)
             {
-                start_match();
+                start_match(lobby);
                 return;
             }
 
@@ -50,8 +50,15 @@ void lobby_scene::wait_player_ready(const shared_ptr<client>& client_ptr, bool r
     });
 }
 
-void lobby_scene::start_match() const
+void lobby_scene::start_match(const domain::lobby_domain& lobby) const
 {
+    for (int i = 3; i > 0; i--)
+    {
+        print_lobby(lobby);
+        cout << "Game starting in...\t" << to_string(i) << endl;
+        this_thread::sleep_for(1.5s);
+    }
+    
     level_scene level_scene(client_ptr_);
     level_scene.show();
 }
@@ -66,5 +73,4 @@ void lobby_scene::print_lobby(domain::lobby_domain lobby) const
         cout << player_lobby.get_name() << "                     " << player_lobby.get_ready_text() << endl;
     }
     cout << "-----------------------------------------" << endl;
-    cout << "Type [OK] to toggle the ready state" << endl;
 }
