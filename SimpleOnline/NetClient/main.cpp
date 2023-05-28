b440f8f05b0fdc33b720cca6b103dea419627bb9
Caue <cauechavesvicente@gmail.com>
2023-05-28 20:47:08 -0300

    add std on netclient main

--- SimpleOnline/NetClient/main.cpp
+++ SimpleOnline/NetClient/main.cpp
@@ -11,18 +11,18 @@ void handle_connection_error(error_code_type error_code)
     switch (error_code)
     {
     case error_code_type::server_full:
-        cout << "Server full, please wait for someone to disconnect";
+        std::cout << "Server full, please wait for someone to disconnect";
         break;
     default:
-        cout << "Couldn't connect to server. Unknown error";
+        std::cout << "Couldn't connect to server. Unknown error";
         break;
     }
 }
 
 int main()
 {
-    cout << "<-~- . - ~-> DUNGEON CRAWLER <-~- . - ~->" << endl << endl;
-    auto client_ptr = make_shared<client>();
+    std::cout << "<-~- . - ~-> DUNGEON CRAWLER <-~- . - ~->" << std::endl << std::endl;
+    auto client_ptr = std::make_shared<client>();
     client_ptr->connect("192.168.18.30", 60000, [&client_ptr](const model::simple_answer_model response)
     {
         if (!response.ok)
